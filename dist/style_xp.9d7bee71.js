9c6f8c93b89dd43ad5b09b23bf295c82c89bcbf2
ideloumeaux <ines.deloumeaux@hetic.net>
2019-03-13 17:05:45 +0100

    homepage done

--- dist/style_xp.9d7bee71.js
+++ dist/style_xp.9d7bee71.js
@@ -171,7 +171,7 @@ function reloadCSS() {
 }
 
 module.exports = reloadCSS;
-},{"./bundle-url":"../node_modules/parcel/src/builtins/bundle-url.js"}],"scss/style.scss":[function(require,module,exports) {
+},{"./bundle-url":"../node_modules/parcel/src/builtins/bundle-url.js"}],"scss/style_xp.scss":[function(require,module,exports) {
 var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
